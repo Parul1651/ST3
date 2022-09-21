505f48524419b840cbc50d5ff2fec6a840e1579e
Parul Abi Goyal <parul1651.be20@chitkara.edu.in>
2022-09-21 15:51:57 +0530

    New Changes commited

---
+++ script.js
@@ -0,0 +1,260 @@
+
+
+
+function click1(){
+
+var a = document.getElementById('n1').value;
+a = Number(a);
+
+
+if(isNaN(a) || a==' ' ){
+    alert('enter valid weight!')
+}else{
+var grav  =0.37;
+var newWt = a * grav;
+document.getElementById('mer').innerHTML = "MERCURY: " + newWt ;
+
+// let pop = document.getElementById();
+
+
+}
+
+
+
+
+
+
+}
+
+
+
+function click2(){
+
+    var a = document.getElementById('n1').value;
+    a = Number(a);
+    
+    
+    if(isNaN(a) || a==' ' ){
+        alert('enter valid weight!')
+    }else{
+    var grav  =0.91;
+    var newWt = a * grav;
+    document.getElementById('ven').innerHTML = "VENUS: " + newWt ;
+    
+    // let pop = document.getElementById();
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    }
+
+    //Earth ka theek karna hai
+    
+function click3(){
+
+var a = document.getElementById('n1').value;
+a = Number(a);
+
+
+if(isNaN(a) || a==' ' ){
+    alert('enter valid weight!')
+}else{
+
+var newWt = a ;
+document.getElementById('ear').innerHTML = "EARTH: " + newWt ;
+
+// let pop = document.getElementById();
+
+
+}
+
+
+
+
+
+
+}
+
+    
+
+function click4(){
+
+    var a = document.getElementById('n1').value;
+    a = Number(a);
+    
+    
+    if(isNaN(a) || a==' ' ){
+        alert('enter valid weight!')
+    }else{
+    var grav  =0.38;
+    var newWt = a * grav;
+    document.getElementById('mar').innerHTML = "MARS: " + newWt ;
+    
+    // let pop = document.getElementById();
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    }
+    
+
+
+
+
+
+function click5(){
+
+    var a = document.getElementById('n1').value;
+    a = Number(a);
+    
+    
+    if(isNaN(a) || a==' ' ){
+        alert('enter valid weight!')
+    }else{
+    var grav  =2.34;
+    var newWt = a * grav;
+    document.getElementById('jup').innerHTML = "JUPITER: " + newWt ;
+    
+    // let pop = document.getElementById();
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    }
+    
+
+
+
+
+function click6(){
+
+    var a = document.getElementById('n1').value;
+    a = Number(a);
+    
+    
+    if(isNaN(a) || a==' ' ){
+        alert('enter valid weight!')
+    }else{
+    var grav  =0.93;
+    var newWt = a * grav;
+    document.getElementById('sat').innerHTML = "SATURN: " + newWt ;
+    
+    // let pop = document.getElementById();
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    }
+    
+
+
+
+
+
+function click7(){
+
+    var a = document.getElementById('n1').value;
+    a = Number(a);
+    
+    
+    if(isNaN(a) || a==' ' ){
+        alert('enter valid weight!')
+    }else{
+    var grav  =0.92;
+    var newWt = a * grav;
+    document.getElementById('urn').innerHTML = "URANUS: " + newWt ;
+    
+    // let pop = document.getElementById();
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    }
+    
+
+
+
+
+
+function click8(){
+
+    var a = document.getElementById('n1').value;
+    a = Number(a);
+    
+    
+    if(isNaN(a) || a==' ' ){
+        alert('enter valid weight!')
+    }else{
+    var grav  =1.12;
+    var newWt = a * grav;
+    document.getElementById('nep').innerHTML = "NEPTUNE: " + newWt ;
+    
+    // let pop = document.getElementById();
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    }
+    
+
+
+
+
+
+
+function click9(){
+
+    var a = document.getElementById('n1').value;
+    a = Number(a);
+    
+    
+    if(isNaN(a) || a==' ' ){
+        alert('enter valid weight!')
+    }else{
+    var grav  =0.083;
+    var newWt = a * grav;
+    document.getElementById('plu').innerHTML = "PLUTO: " + newWt ;
+    
+    // let pop = document.getElementById();
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    }
+    
+    
